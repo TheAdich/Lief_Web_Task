47ac97adb76dbb72025cc199709c76dd2468a342
Aditya Dadhich <adidadhich2003@gmail.com>
2024-09-16 10:41:43 +0530

    added_auth

---
+++ app/api/users/register/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export async function POST(req: Request) {
+    const { username, email } = await req.json();
+    if (!username || !email) return NextResponse.json({ message: 'Fill all the required fields' }, { status: 409 })
+    if (req.method === 'POST') {
+        const checkIfUserExists = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+        if (!checkIfUserExists) {
+            const user = await prisma.user.create({
+                data: {
+                    username: username,
+                    email: email,
+                }
+            })
+            console.log(user);
+            if (user) return NextResponse.json(user, { status: 200 });
+        }
+    }
+    else {
+        return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
+    }
+}
