b4bf44e799794f0b41d4e62441c04b3eba488876
aditya_dadhich <128921226+TheAdich@users.noreply.github.com>
2024-10-02 16:04:08 +0530

    Update page.tsx

--- app/registerinjury/page.tsx
+++ app/registerinjury/page.tsx
@@ -86,8 +86,8 @@ useEffect(() => {
 
     return (
         <div className="w-3/4 mx-auto p-6 mt-10 bg-opacity-30 bg-slate-200 rounded-xl shadow-xl backdrop-blur-md">
-            <h1 className="text-md font-bold text-white mb-4 text-center">Injury Data Form</h1>
-            <p className="font-medium text-sm">Just fill it completely</p>
+           <h1 className="text-2xl font-extrabold text-blue-700 mb-6 text-right">Accident Data Form</h1>
+<p className="font-light text-xs text-green-400">Complete the form thoroughly</p>
             <form className="space-y-4" onSubmit={(e) => handleFormSubmit(e)}>
                 <div>
                     <label className="block text-white text-sm font-medium mb-1">Reporter</label>
